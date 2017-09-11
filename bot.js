3b96459f3221f1520a40b41279023c0dc5f1dd48
cn27529 <cn27529@hotmail.com>
2017-09-11 17:36:18 +0800

    0911a

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ bot.onText(/\/start/, msg => {
 
     console.log(msg); // for debug
     const chatId = msg.chat.id;
-    bot.sendmsg(chatId, 'Hello World');
+    bot.sendMessage(chatId, 'Hello World');
 
 });
 
@@ -17,7 +17,7 @@ bot.onText(/\/吃飯飯/, msg => {
 
     const chatId = msg.chat.id;
     console.log(chatId); // for debug
-    bot.sendmsg(chatId, '去你的飯飯');
+    bot.sendMessage(chatId, '去你的飯飯');
 
 });
 
@@ -25,15 +25,15 @@ bot.onText(/\/飯飯/, msg => {
 
     const chatId = msg.chat.id;
     console.log(chatId); // for debug
-    bot.sendmsg(chatId, '去你的飯飯');
+    bot.sendMessage(chatId, '去你的飯飯');
 
 });
 
 bot.onText(/\/hi/, msg => {
-    
+
     const chatId = msg.chat.id;
     console.log(chatId); // for debug
-    bot.sendmsg(chatId, '想幹嘛!');
+    bot.sendMessage(chatId, '想幹嘛!');
     console.log(msg.chat.first_name);
     console.log(msg.chat.username);
     //console.log(msg.chat.type);
@@ -43,19 +43,29 @@ bot.onText(/\/hi/, msg => {
 });
 
 bot.onText(/\/help/, msg => {
-    
+
     const chatId = msg.chat.id;
     console.log(chatId); // for debug
-    bot.sendmsg(chatId, '\n111\n222\n333\n');
+    bot.sendMessage(chatId, '\n111\n222\n333\n');
 
 });
 
-// On every text message
-bot.on('text', msg => {
-    let id = msg.from.id;
-    let text = msg.text;
-    return bot.sendMessage(id, `You said: ${ text }`);
+// // On every text message
+// bot.on('text', msg => {
+//     let id = msg.from.id;
+//     let text = msg.text;
+//     return bot.sendMessage(id, `You said: ${ text }`);
+// });
 
-    return bot.sendMessage(id, `You said: ${ text }`);
-    
+bot.onText(/\/weather/, msg => {
+
+    const chatId = msg.chat.id;
+    console.log(chatId); // for debug
+    bot.sendMessage(chatId, '天氣資料己處理!');
+    console.log(msg.chat.first_name);
+    console.log(msg.chat.username);
+    //console.log(msg.chat.type);
+    //console.log(msg.chat.text);
+
+
 });
