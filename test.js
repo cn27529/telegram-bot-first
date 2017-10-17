9a6f046c0d40c5a0e37cde072a748a0598fe00b4
cn27529 <cn27529@hotmail.com>
2017-10-17 20:47:41 +0800

    1017

--- test.js
+++ test.js
@@ -4,3 +4,24 @@ var hi_value = helloworld.sayHi('hiiiiiiiiii')
 
 console.log(hi_value)
 
+
+
+var firebase = require('firebase');
+
+let config = {
+    apiKey: "AIzaSyAg0tGWsKqtjZcRnFPrBroVn8r111Q4EKY",
+    authDomain: "bkappdb.firebaseapp.com",
+    databaseURL: "https://bkappdb.firebaseio.com",
+    projectId: "bkappdb",
+    storageBucket: "bkappdb.appspot.com",
+    messagingSenderId: "1087681092838"
+};
+firebase.initializeApp(config);
+
+// msgRef = firebase中的資料表/messages/，若沒有的會自動建立
+const msgRef = firebase.database().ref('/messages/');
+msgRef.on('value', function(snapshot) {
+    const val = snapshot.val();
+    //vm.messages = val;
+    console.log(val)
+})
